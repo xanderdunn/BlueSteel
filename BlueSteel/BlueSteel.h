b97672d691b2af723e0866a10694368047f33a5f
Matt Isaacs <misaacs@gilt.com>
2014-09-03 16:27:24 -0400

    Initial Commit

---
+++ BlueSteel/BlueSteel.h
@@ -0,0 +1,19 @@
+//
+//  BlueSteel.h
+//  BlueSteel
+//
+//  Created by Matt Isaacs
+//  Copyright (c) 2014 Gilt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlueSteel.
+FOUNDATION_EXPORT double BlueSteelVersionNumber;
+
+//! Project version string for BlueSteel.
+FOUNDATION_EXPORT const unsigned char BlueSteelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueSteel/PublicHeader.h>
+
+
