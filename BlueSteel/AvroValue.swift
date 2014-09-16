7e14531fb167b548e3c2a963aca689edf8d364a9
Matt Isaacs <misaacs@gilt.com>
2014-09-16 14:23:26 -0400

    Concat dictionaries.

--- BlueSteel/AvroValue.swift
+++ BlueSteel/AvroValue.swift
@@ -9,6 +9,17 @@
 import Foundation
 
 
+extension Dictionary {
+
+}
+
+func +=<K, V> (inout left: Dictionary<K, V>, right: Dictionary<K, V>) -> Dictionary<K, V> {
+    for (k, v) in right {
+        left.updateValue(v, forKey: k)
+    }
+    return left
+}
+
 public enum AvroValue {
     // Primitives
     case AvroNullValue
