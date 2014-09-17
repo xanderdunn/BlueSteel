4b40fabedd7cadc46760191178f26cdd76190880
Matt Isaacs <misaacs@gilt.com>
2014-09-17 13:36:11 -0400

    Encoder stubs.

---
+++ BlueSteel/AvroEncoder.swift
@@ -0,0 +1,59 @@
+//
+//  AvroEncoder.swift
+//  BlueSteel
+//
+//  Created by Matt Isaacs.
+//  Copyright (c) 2014 Gilt. All rights reserved.
+//
+
+import Foundation
+
+public class AvroEncoder {
+
+    var bytes: [Byte] = []
+
+    func encodeNull() {
+        return
+    }
+
+    func encodeBoolean() {
+        // Stub
+        return
+    }
+
+    func encodeInt() {
+        // Stub
+        return
+    }
+
+    func encodeLong() {
+        // Stub
+        return
+    }
+    
+    func encodeFloat() {
+        // Stub
+        return
+    }
+    
+    func encodeDouble() {
+        // Stub
+        return
+    }
+
+    func encodeString() {
+        // Stub
+        return
+    }
+
+    func encodeBytes() {
+        // Stub
+        return
+    }
+
+    func encodeFixed(size: Int) {
+        // Stub
+        return
+    }
+    
+}
