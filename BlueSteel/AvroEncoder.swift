9d1ebcf1e14088eae545ea3456fb3c5a4e1ba076
Matt Isaacs <misaacs@gilt.com>
2014-09-18 00:57:58 -0400

    Avro primitive encoder. Added to project.

--- BlueSteel/AvroEncoder.swift
+++ BlueSteel/AvroEncoder.swift
@@ -16,43 +16,64 @@ public class AvroEncoder {
         return
     }
 
-    func encodeBoolean() {
-        // Stub
+    func encodeBoolean(value: Boolean) {
+        bytes.append(Byte(value))
         return
     }
 
-    func encodeInt() {
-        // Stub
+    func encodeInt(value: Int32) {
+        let encoded = Varint(fromValue: Int64(value))
+        bytes += encoded.backing
         return
     }
 
-    func encodeLong() {
-        // Stub
+    func encodeLong(value: Int64) {
+        let encoded = Varint(fromValue: UInt64(value))
+        bytes += encoded.backing
         return
     }
     
-    func encodeFloat() {
-        // Stub
+    func encodeFloat(value: Float) {
+        let bits: UInt64 = unsafeBitCast(value, UInt64.self)
+
+        bytes.append(Byte(0xff & (bits >> 24)))
+        bytes.append(Byte(0xff & (bits >> 16)))
+        bytes.append(Byte(0xff & (bits >> 8)))
+        bytes.append(Byte(0xff & bits))
         return
     }
     
-    func encodeDouble() {
-        // Stub
+    func encodeDouble(value: Double) {
+        let bits: UInt64 = unsafeBitCast(value, UInt64.self)
+
+        bytes.append(Byte(0xff & (bits >> 56)))
+        bytes.append(Byte(0xff & (bits >> 48)))
+        bytes.append(Byte(0xff & (bits >> 40)))
+        bytes.append(Byte(0xff & (bits >> 32)))
+        bytes.append(Byte(0xff & (bits >> 24)))
+        bytes.append(Byte(0xff & (bits >> 16)))
+        bytes.append(Byte(0xff & (bits >> 8)))
+        bytes.append(Byte(0xff & bits))
         return
     }
 
-    func encodeString() {
-        // Stub
+    func encodeString(value: String) {
+        var stringBytes: [Byte] = []
+        for char in value.utf8 {
+            stringBytes.append(char)
+        }
+        encodeBytes(stringBytes)
         return
     }
 
-    func encodeBytes() {
-        // Stub
+    func encodeBytes(value: [Byte]) {
+        encodeLong(Int64(value.count))
+        bytes += value
         return
     }
 
-    func encodeFixed(size: Int) {
-        // Stub
+    func encodeFixed(value: [Byte]) {
+        bytes += value
         return
     }
     
